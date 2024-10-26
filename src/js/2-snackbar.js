121117a711bdf27b7f80adda564ccbaf87764b30
Borys Ziukov <ziukoff1985@gmail.com>
2024-10-27 01:02:31 +0300

    added solving of task #2

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,46 @@
-console.log('snackbar');
+'use strict';
+
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const delay = parseInt(event.target.delay.value);
+
+  const state = event.target.state.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  form.reset();
+
+  promise
+    .then(delay => {
+      iziToast.success({
+        title: '✅ Fulfilled',
+        message: `promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: '❌ Rejected',
+        message: `promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+}
